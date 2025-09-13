c5773539dc29be86538a9063824a0a8681e35658
xtasysensei <maiwuyashem@gmail.com>
2025-09-13 09:01:19 +0100

    working on error handling and agrument handling

--- main.c
+++ main.c
@@ -2,14 +2,25 @@
 
 struct BytesBuffer {
     char bytes[];
-}
+};
 
-int main(int args, char *argv[]){
+int main(int args, char** argv){
 
-    char file_name = *argv[1];
+    char* file_name = argv[1];
+    // size_t length = sizeof(argv) / sizeof(argv[0]);
+
+    // if (length > 1 || length == 0) {
+    //     printf("too many arguments");
+    // }
+    if (args > 2) {
+        printf("Too many arguments");
+
+    } else if(args < 2) {
+        printf("Not enough arguments");
+    }
 
     FILE *ptr = fopen(file_name, "rb");
-    if (ptr == null){
+    if (ptr == NULL){
         printf("read failed");
         return 1;
     }
